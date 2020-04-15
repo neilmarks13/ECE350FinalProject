c742a488e938d84064bb7184020a3f84628d7fee
neilmarks13 <60155165+neilmarks13@users.noreply.github.com>
2020-04-15 16:53:38 -0400

    Add files via upload

---
+++ math.s
@@ -0,0 +1,23 @@
+.text 
+
+###
+### ### ECE350: Processor Checkpoint
+### ### Basic Math Test
+### ### MIPS
+###
+
+add     $r0, $r0, $r0
+add     $r0, $r0, $r0
+add     $r0, $r0, $r0
+
+addi    $r1, $r1, 100
+addi    $r2, $r2, 200
+addi    $r3, $r3, 300
+addi    $r4, $r4, 400
+addi    $r5, $r5, 500
+
+add     $r6, $r1, $r2
+sub     $r7, $r3, $r2
+addi    $r8, $r4, 400
+sll     $r9, $r1, 5
+sra     $r10, $r2, 2
